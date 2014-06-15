c4b2d14d117205379ec2f09d5922648f3ee0452a
DunKuai Su <sudunkuai@gmail.com>
2014-06-15 13:01:24 +0800

    123
    123

--- protected/config/params.php
+++ protected/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    'upload_file_path'=>'/Applications/XAMPP/htdocs/news/images/attach',
+    'upload_file_path'=>'/opt/www/root/news/images/attach',
     'upload_file_maxsize'=>'4',
     'assets_path'=>"images/attach/",
 );
